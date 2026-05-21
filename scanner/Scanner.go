bf38be2a7063de5b022839a33f81f333ecb491a5
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2026-05-22 02:25:32 +0530

    feat: day 3 - implement 2 chars, str/num literals

--- scanner/Scanner.go
+++ scanner/Scanner.go
@@ -1,6 +1,11 @@
 package scanner
 
-import "github.com/sunguru98/glox/lib"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/sunguru98/glox/lib"
+)
 
 type Scanner struct {
 	source  string  // The raw source code
@@ -21,50 +26,116 @@ func InitScanner(source string) *Scanner {
 	}
 }
 
-func (s *Scanner) ScanToken() {
+func (s *Scanner) scanToken() {
 	// Get the current character
-	currentCharacter := s.FetchCharAndAdvance()
+	currentCharacter := s.consumeCharAndAdvance()
 
 	// And parse the token based on the fetched character
 	switch currentCharacter {
+	// Single character lexemes
 	case '(':
-		s.AddToken(LeftParen)
+		s.addToken(LeftParen)
 	case ')':
-		s.AddToken(RightParen)
+		s.addToken(RightParen)
 	case '{':
-		s.AddToken(LeftBrace)
+		s.addToken(LeftBrace)
 	case '}':
-		s.AddToken(RightBrace)
+		s.addToken(RightBrace)
 	case ',':
-		s.AddToken(Comma)
+		s.addToken(Comma)
 	case '.':
-		s.AddToken(Dot)
+		s.addToken(Dot)
 	case '-':
-		s.AddToken(Minus)
+		s.addToken(Minus)
 	case '+':
-		s.AddToken(Plus)
+		s.addToken(Plus)
 	case ';':
-		s.AddToken(Semicolon)
+		s.addToken(Semicolon)
 	case '*':
-		s.AddToken(Star)
-	// When none of the assumed characters match, invoke an error
+		s.addToken(Star)
+
+	// The following lexemes can either be single or double
+	case '!':
+		var tType TokenType = Bang
+		if s.matchCharAndAdvance('=') {
+			tType = BangEqual
+		}
+		s.addToken(tType)
+	case '=':
+		var tType TokenType = Equal
+		if s.matchCharAndAdvance('=') {
+			tType = EqualEqual
+		}
+		s.addToken(tType)
+	case '<':
+		var tType TokenType = Less
+		if s.matchCharAndAdvance('=') {
+			tType = LessEqual
+		}
+		s.addToken(tType)
+	case '>':
+		var tType TokenType = Greater
+		if s.matchCharAndAdvance('=') {
+			tType = GreaterEqual
+		}
+		s.addToken(tType)
+
+	// '/' can either be division or comment '//'.
+	case '/':
+		if s.matchCharAndAdvance('/') {
+			// The whole line is just a comment, hence advance till a new line ('\n') is met
+			for {
+				// If either we meet with the new line character
+				// Or the end of file, break.
+				// For new line, we have a seperate case handling it
+				// Hence the '\n' is not consumed but peeked instead
+				if s.peek() == '\n' || s.isAtEnd() {
+					break
+				}
+
+				// Until then, we advance and consume those characters through current index increment
+				s.consumeCharAndAdvance()
+			}
+		} else {
+			// Else it's normal division lexeme
+			s.addToken(Slash)
+		}
+
+	// No tokens are added for anything whitespace related
+	case ' ', '\r', '\t':
+		break
+
+	// For new line, the line value is incremented
+	case '\n':
+		s.line++
+
+	// String literals
+	case '"':
+		s.matchString()
+
 	default:
+		// Check if it's a numeric literal
+		if s.isDigit(currentCharacter) {
+			s.matchDigit()
+			break
+		}
+
+		// When none of the assumed character combinations match, invoke an error
 		lib.Error(s.line, "Unexpected character.")
-		return
 	}
 }
 
 func (s *Scanner) ScanTokens() {
 	for {
 		// If reached end of source break
-		if s.IsAtEnd() {
+		if s.isAtEnd() {
 			break
 		}
 
 		// We start from the beginning character of the lexeme. s.current is where the beginning is
 		s.start = s.current
 		// Scan and add token to list
-		s.ScanToken()
+		s.scanToken()
 	}
 
 	// Since we've reached EOF, need to add EOF token
@@ -72,12 +143,48 @@ func (s *Scanner) ScanTokens() {
 	s.tokens = append(s.tokens, *eofToken)
 }
 
-func (s *Scanner) IsAtEnd() bool {
+func (s *Scanner) addToken(tokenType TokenType) {
+	s.addTokenWithLiteral(tokenType, nil)
+}
+
+func (s *Scanner) addTokenWithLiteral(tokenType TokenType, literal any) {
+	// We fetch the raw character blob (lexeme) and mark the corresponding token type and the parsed literal (if any)
+	lexeme := s.source[s.start:s.current]
+	// Creating and adding it into the token list
+	token := NewToken(tokenType, lexeme, literal, s.line)
+	s.tokens = append(s.tokens, *token)
+}
+
+func (s *Scanner) isAtEnd() bool {
 	// Check if the cursor of "current" offset has reached end of source code
 	return s.current >= len(s.source)
 }
 
-func (s *Scanner) FetchCharAndAdvance() byte {
+func (s *Scanner) peek() byte {
+	// If we've reached EOF, then return null terminator
+	if s.isAtEnd() {
+		return '\x00'
+	}
+
+	// Otherwise peek (look at current index's character of the source)
+	peekedCharacter := s.source[s.current]
+	return peekedCharacter
+}
+
+func (s *Scanner) peekNext() byte {
+	// Next character is current index + 1
+	// We aren't mutating s.current, since this is peek, not consume
+	nextCharacter := s.current + 1
+	// Same like peek() .. Return null terminator if EOF
+	if nextCharacter >= len(s.source) {
+		return '\x00'
+	}
+
+	peekedCharacter := s.source[nextCharacter]
+	return peekedCharacter
+}
+
+func (s *Scanner) consumeCharAndAdvance() byte {
 	// Ingest/Fetch/Consume the current character and move the cursor by 1 byte/character
 	character := s.source[s.current]
 	s.current++
@@ -85,14 +192,111 @@ func (s *Scanner) FetchCharAndAdvance() byte {
 	return character
 }
 
-func (s *Scanner) AddToken(tokenType TokenType) {
-	s.AddTokenWithLiteral(tokenType, nil)
+func (s *Scanner) matchCharAndAdvance(expectedChar byte) bool {
+	// If the current cursor is already at the source end
+	// then there are no more characters to check
+	if s.isAtEnd() {
+		return false
+	}
+
+	// Check if the current character matches the expected character
+	currentCharacter := s.source[s.current]
+	if currentCharacter != expectedChar {
+		return false
+	}
+
+	// If it does, consume the character and move forward by 1 character/byte
+	s.current++
+
+	return true
 }
 
-func (s *Scanner) AddTokenWithLiteral(tokenType TokenType, literal any) {
-	// We fetch the raw character blob (lexeme) and mark the corresponding token type and the parsed literal (if any)
-	lexeme := s.source[s.start:s.current]
-	// Creating and adding it into the token list
-	token := NewToken(tokenType, lexeme, literal, s.line)
-	s.tokens = append(s.tokens, *token)
+func (s *Scanner) matchString() {
+	for {
+		// The loop advances until the current index is at end of source
+		// Or current index points the character '"' (closing quote for the string)
+		if s.isAtEnd() || s.peek() == '"' {
+			break
+		}
+
+		// The programming language supports multiline strings
+		// Hence we also update the line number state accordingly
+		if s.peek() == '\n' {
+			s.line++
+		}
+
+		s.consumeCharAndAdvance()
+	}
+
+	// The loop might be terminated due to two causes
+	// 1. If current index is at end of source, without closing quote (")
+	// Which means it's not a valid string
+	if s.isAtEnd() {
+		lib.Error(s.line, "Unterminated string")
+		return
+	}
+
+	// 2. The healthy loop termination is when it has peeked the closing quote
+	// Which means, we consume '"' and advance
+	s.consumeCharAndAdvance()
+
+	// Before this function, start and current would be in the same place.
+	// Now start and current are in beginning and after end of the string.
+	// For an example string like "apple" .. start would be at '"' (open quote)
+	// And current would be after the closing quote. (since we consumed it)
+	// Hence we have to trim the quotes for the actual value
+	strStart := s.start + 1     // Go one char after open quote
+	strCurrent := s.current - 1 // Go one char before, to land exactly at the closing quote (since end index is not inclusive)
+
+	// Difference between literal and raw lexeme here is. Lexeme would contain with quotes
+	// Literal value just captures the true type. Here it's the string value inside the quotes.
+	strLiteralValue := s.source[strStart:strCurrent]
+
+	// We then create this as a token and add it to the list
+	s.addTokenWithLiteral(String, strLiteralValue)
+}
+
+func (s *Scanner) isDigit(charToBeChecked byte) bool {
+	return charToBeChecked >= '0' && charToBeChecked <= '9'
+}
+
+func (s *Scanner) consumeDigits() {
+	for {
+		// The loop advances till the current index isn't pointing a number
+		peekedCharacter := s.peek()
+		if !s.isDigit(peekedCharacter) {
+			break
+		}
+
+		s.consumeCharAndAdvance()
+	}
+}
+
+func (s *Scanner) matchDigit() {
+	// A number can be either multiple digits (1234)
+	// Or with decimals (123.456)
+	s.consumeDigits()
+
+	// The loop inside `consumeDigits` might be terminated midway if current index points to '.'
+	// Hence we check if that is the case and also check if current + 1 index points to a number
+	currentPeekedCharacter := s.peek()
+	nextPeekedCharacter := s.peekNext()
+	if currentPeekedCharacter == '.' && s.isDigit(nextPeekedCharacter) {
+		// Consume the decimal dot
+		s.consumeCharAndAdvance()
+
+		// Consume the remaining digits after the decimal dot
+		s.consumeDigits()
+	}
+
+	// Since there are no quotes surrounding a number, we can take start and current as is
+	// The programming language implicitly considers all numbers as f64 for interpreter simplicity
+	// Hence we parse it as an f64 and then create a token and add it to list
+	valueStr := s.source[s.start:s.current]
+	value, err := strconv.ParseFloat(valueStr, 64)
+	if err != nil {
+		lib.Error(s.line, fmt.Sprintf("Failed to format digit. Error: %v", err))
+	}
+
+	s.addTokenWithLiteral(Number, value)
 }
